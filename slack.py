336cc470f82d15586b9a5ef2b163a7037a8e1f8a
DeltaStruct <DeltaStruct47@gmail.com>
2023-05-27 12:45:23 +0900

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,21 +1,16 @@
 import os
 import requests as api
-from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+import os
+from slack_sdk.web import WebClient
 class slack_api:
     def __init__(self):
-        #アプリの初期化
-        self.app = App(token=os.environ['Slack_BOT_API_Key'])
-        SocketModeHandler(self.app, os.environ['Slack_APP_API_Key']).start()
+        self.client = WebClient(token=os.environ["Slack_API_Key"])
     def test(self):
-        time_now = datetime.datetime.now().strftime("%H時%M分%S秒")
-        say("test\n現在時刻" + time_now)
-    def send(self, mes):
-        say(mes)
+        self.response = self.client.chat_postMessage(text=":wave: こんにちは！", channel="#random")
+        print(self.response)
 def main():
-    slackapi=slack_api()
-    slackapi.test()
+    slack = slack_api()
+    slack.test()
 if __name__ == "__main__":
-   print(os.environ["SSL_CERT_FILE"]) 
-  #main()
+    main()
