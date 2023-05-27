febaa7db175d37b5a8208eaba26975de0e0db3c0
DeltaStruct <DeltaStruct47@gmail.com>
2023-05-27 13:41:33 +0900

    Update slack.py

--- slack.py
+++ slack.py
@@ -6,19 +6,15 @@ import sys
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
  
-class slack_api:
-    def __init__(self):
-        self.app = App(token=os.environ["Slack_BOT_API_Key"])
-        SocketModeHandler(self.app, os.environ["Slack_APP_API_Key"]).start()
-    @app.message('test')
-    def test(self):
-        time_now = datetime.datetime.now().strftime("%H時%M分%S秒")
-        say("test\n現在時刻:" + time_now)
-    def send(self, msg):
-        say(msg)
+app = App(token=os.environ["Slack_BOT_API_Key"])
+@app.message('test')
+def test(self):
+    time_now = datetime.datetime.now().strftime("%H時%M分%S秒")
+    say("test\n現在時刻:" + time_now)
+def send(self, msg):
+    say(msg)
 def main():
-    slack = slack_api()
-    slack.test()
+    SocketModeHandler(self.app, os.environ["Slack_APP_API_Key"]).start()
 if __name__ == "__main__":
     main()
     sys.exit(0)
