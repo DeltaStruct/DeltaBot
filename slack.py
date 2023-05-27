008aba4f4593174eef3e2648024f051d3a1b9b90
DeltaStruct <DeltaStruct47@gmail.com>
2023-05-27 13:05:51 +0900

    Update slack.py

--- slack.py
+++ slack.py
@@ -1,14 +1,19 @@
 import os
 import requests as api
+import datetime
 
-import os
-from slack_sdk.web import WebClient
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
 class slack_api:
     def __init__(self):
-        self.client = WebClient(token=os.environ["Slack_BOT_API_Key"])
+        self.app = App(token=os.environ["Slack_BOT_API_Key"])
+        SocketModeHandler(self.app, os.environ["Slack_APP_API_Key"]).start()
     def test(self):
-        self.response = self.client.chat_postMessage(text=":wave: こんにちは！", channel="#random")
-        print(self.response)
+        time_now = datetime.datetime.now().strftime("%H時%M分%S秒")
+        say("test\n現在時刻:" + time_now)
+    def send(self, msg):
+        say(msg)
 def main():
     slack = slack_api()
     slack.test()
