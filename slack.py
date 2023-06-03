9d8930844aa9aae4541b4d3f8d9fed47868b65b5
DeltaStruct <DeltaStruct47@gmail.com>
2023-06-03 17:35:46 +0900

    Update and rename slack_ws.py to slack.py

--- slack.py
+++ slack.py
@@ -1,10 +1,10 @@
 import os
 import requests as api
 import datetime
-import sys
 
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk.webhook import Webhook_Slack
  
 app = App(token=os.environ["Slack_BOT_API_Key"])
 @app.message('test')
@@ -14,6 +14,14 @@ def test(message, say):
     say("test\n現在時刻:" + time_now)
 def main():
     SocketModeHandler(app, os.environ["Slack_APP_API_Key"]).start()
+
+class http:
+    def __init__(self, ch:"string"):
+        self.ch = str(ch)
+        self.client = Webhook_Slack(token=os.environ["Slack_BOT_API_Key"])
+    
+    def send(self, msg:"string"):
+        self.client.chat_postMessage(text=msg, channel=self.ch)
+        
 if __name__ == "__main__":
     main()
-    sys.exit(0)
